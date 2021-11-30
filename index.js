5e69ab993847161703b8dfb069eda9fa31fb0cde
Mrunal-13 <mrunalnimbalkar13032001@gmail.com>
2021-11-30 22:19:14 +0530

    play me feature

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
-var randomnumber1=Math.floor(Math.random()*6)+1;//1-6
-var randomdiceimage1="dice"+randomnumber1+".png";
-var randomimage1="images/"+randomdiceimage1;//images/dice1.png - images/dice6.png
 
-var image1=document.querySelectorAll("img")[0];
-image1.setAttribute("src",randomimage1);
+
+
+//document.querySelector("h1").innerHTML="Refresh Me"
+
+var randomnumber1=Math.floor(Math.random()*6)+1;//1-6
+
+console.log(randomnumber1)
+
 
 var randomnumber2=Math.floor(Math.random()*6)+1;//1-6
-var randomdiceimage2="dice"+randomnumber2+".png";
-var randomimage2="images/"+randomdiceimage2;//images/dice1.png - images/dice6.png
 
-var image2=document.querySelectorAll("img")[1];
-image2.setAttribute("src",randomimage2);
+console.log(randomnumber2)
 
-if(randomnumber1>randomnumber2){
-    document.querySelector("h1").innerHTML="congrats! player1 Wins";
-}
-else if(randomnumber1<randomnumber2)
-{
-    document.querySelector("h1").innerHTML="Player 2 wins";
-}
-else{
-    document.querySelector("h1").innerHTML="Draw !"
+function play(){
+    if(randomnumber1>randomnumber2){
+        var randomdiceimage1="dice"+randomnumber1+".png";
+        var randomimage1="images/"+randomdiceimage1;//images/dice1.png - images/dice6.png
+        var image1=document.querySelectorAll("img")[0];
+        image1.setAttribute("src",randomimage1);
+        var randomdiceimage2="dice"+randomnumber2+".png";
+        var randomimage2="images/"+randomdiceimage2;//images/dice1.png - images/dice6.png
+        var image2=document.querySelectorAll("img")[1];
+        image2.setAttribute("src",randomimage2);
+
+        document.querySelector("h1").innerHTML="congrats! player1 Wins";
+    }
+    else if(randomnumber1<randomnumber2)
+    
+    {
+        var randomdiceimage1="dice"+randomnumber1+".png";
+        var randomimage1="images/"+randomdiceimage1;//images/dice1.png - images/dice6.png
+        var image1=document.querySelectorAll("img")[0];
+        image1.setAttribute("src",randomimage1);
+        var randomdiceimage2="dice"+randomnumber2+".png";
+        var randomimage2="images/"+randomdiceimage2;//images/dice1.png - images/dice6.png
+        var image2=document.querySelectorAll("img")[1];
+
+        image2.setAttribute("src",randomimage2);
+        document.querySelector("h1").innerHTML="Player 2 wins";
+    }
+    else{
+        document.querySelector("h1").innerHTML="Draw !"
+    }
 }
